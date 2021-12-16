c7dbdcdb4a9ca658906f02c087196990c1da6676
Karan Mann <karan.mann@outlook.com>
2021-12-16 15:42:34 +0100

    weather and news functions added

---
+++ scripts/news.js
@@ -0,0 +1,39 @@
+const NEWS_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=88804fd27daf4133b20f9cd7037177bf"
+const displayNews = document.getElementById("latest-news")
+
+
+const fetchNewsData = () => {
+
+  fetch(NEWS_URL)
+    .then(res => res.json())
+    .then(newsData => {
+      displayNewsCards(newsData)
+    })
+}
+
+fetchNewsData()
+
+const displayNewsCards = (newsData) => {
+  let newsOutput = "";
+
+  newsData.articles.map((news) => {
+    
+    let d = new Date(news.publishedAt);
+    let hours = d.getUTCHours(); // Hours
+    let minutes = d.getUTCMinutes();
+    newsOutput += `
+        <a class="news-card" href=${news.url} target="default">
+          <img class="news-image" src=${news.urlToImage == undefined ? "../assets/sr01-700x217.jpeg" : news.urlToImage} alt=${news.name}>
+          <div class="news-card-content">
+            <h4>${news.title}</h4>
+            <p>${news.description}</p>
+            <div>
+              <p><b>Source:</b> ${news.source.name}</p>
+              <p><b>Published:</b> ${hours}:${minutes}</p>
+            </div>
+          </div>
+        </a>
+    `;
+    displayNews.innerHTML = newsOutput;
+  });
+}
---
+++ scripts/weather.js
@@ -0,0 +1,48 @@
+const API_ONE_DAY = `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=a77136eb90fd8d858de1f60c1ae1ee51`;
+
+const setWeatherData = document.getElementById("weather");
+const fetchWeatherData = () => {
+  fetch(API_ONE_DAY)
+    .then((res) => res.json())
+    .then((weatherData) => displayWeatherData(weatherData));
+};
+
+fetchWeatherData();
+
+displayWeatherData = (weatherData) => {
+ 
+  const sunriseHour = new Date(weatherData.sys.sunrise * 1000).getHours();
+  const sunriseMinutes = "0" + new Date(weatherData.sys.sunrise * 1000).getMinutes();
+  const formattedSunrise = `${sunriseHour}:${sunriseMinutes.substr(-2)}`;
+
+  const sunsetHour = new Date(weatherData.sys.sunset * 1000).getHours();
+  const sunsetMinutes = "0" + new Date(weatherData.sys.sunset * 1000).getMinutes();
+  const formattedSunset = `${sunsetHour}:${sunsetMinutes.substr(-2)}`;
+
+  const date = new Date(weatherData.dt * 1000);
+  const weekday = date.toLocaleDateString('en-GB', {weekday: 'long'});
+  const fullDate = date.toLocaleDateString('en-GB');
+
+  const feelsLike = Math.round(weatherData.main.feels_like - 273);
+  const maxTemperature = Math.round(weatherData.main.temp_max - 273);
+  const minTemperature = Math.round(weatherData.main.temp_min - 273);
+
+
+  let weatherOutput = "";
+
+  weatherOutput += `
+  <p>${weekday} - ${fullDate}</p>
+  <p>${weatherData.name}, ${weatherData.sys.country} - ${(weatherData.main.temp - 273).toFixed(1)} &#x2103</p>
+  <div class="weather-temp">
+    <p>Feels Like : ${feelsLike} &#x2103</p>
+    <p>Max Temp: ${maxTemperature} &#x2103</p>
+    <p>Min Temp: ${minTemperature} &#x2103</p>
+  </div>
+  <div class="weather-suns">
+    <p>Sunrise : ${formattedSunrise}</p>
+    <p>Sunset : ${formattedSunset}</p>
+  </div>
+  
+  `;
+  setWeatherData.innerHTML = weatherOutput;
+};
